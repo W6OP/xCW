515bda37288b9cb1e6cf13dee717569fef6bdeb0
Peter Bourget <pbourget@w6op.com>
2020-05-11 12:31:23 -0700

    Initial Commit

---
+++ xCW/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  xCW
+//
+//  Created by Peter Bourget on 5/11/20.
+//  Copyright © 2020 Peter Bourget. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
