36449749855993dbfab870151b8c82170c1c5d4f
Peter Bourget <pbourget@w6op.com>
2020-08-08 12:42:45 -0700

    Moved memory panel to separate file.

--- xCW/ContentView.swift
+++ xCW/ContentView.swift
@@ -498,64 +498,8 @@ struct StationPicker: View {
 }
 
 // MARK: - CW Message Panel ----------------------------------------------------------------------------
+// moved to separate file
 
-/**
-View of the CW Memory sheet.
-*/
-//struct CWMemoriesPicker: View {
-//  @Environment(\.presentationMode) var presentationMode
-//  @EnvironmentObject var radioManager: RadioManager
-//  @State private var entry = ""
-//
-//  var body: some View {
-//
-//    return VStack{
-//      HStack{
-//        Text("CW Memory Panel").frame(minWidth: 50).padding(.leading, 5)
-//      }
-//      .font(.system(size: 14))
-//      .foregroundColor(Color.blue)
-//
-//      VStack {
-//        ForEach(radioManager.cwMemoryModels.indices, id: \.self ) { index in
-//          HStack {
-//            Button(action: { self.radioManager.sendCWMessage(tag: self.radioManager.cwMemoryModels[index].tag, freeText: "") }) {
-//              Text(self.radioManager.cwMemoryModels[index].line)
-//                .frame(minWidth: 50, maxWidth: 50)
-//            }
-//            .padding(.leading, 5).padding(.trailing, 5)
-//            .disabled(self.radioManager.sliceModel.radioMode.rawValue != "CW")
-//
-//            // https://www.reddit.com/r/SwiftUI/comments/fauxsb/error_binding_textfield_to_object_in_array/
-//            TextField("Enter Text Here", text: self.$radioManager.cwMemoryModels[index].line,
-//                      onEditingChanged: { _ in
-//                        self.radioManager.saveCWMemory(message:
-//                          self.radioManager.cwMemoryModels[index].line, tag:
-//                          self.radioManager.cwMemoryModels[index].tag); print("TextField changed focus")},
-//                      onCommit: {
-//                        print("Committed!")
-//            })
-//            .textFieldStyle(RoundedBorderTextFieldStyle())
-//          }
-//        }
-//        .frame(minWidth: 400, maxWidth: 400)
-//      }
-//      .frame(minWidth: 450, maxWidth: 450)
-//
-//      // syncs with main panel
-//      HStack {
-//        Text("Set Speed")
-//        Stepper(value: self.$radioManager.cwSpeed, in: 5...80,onEditingChanged: { _ in self.radioManager.saveCWSpeed(speed: self.radioManager.cwSpeed) }, label: { Text("\(self.radioManager.cwSpeed)") })
-//
-//        Button(action: {self.presentationMode.wrappedValue.dismiss()}) {
-//          Text("Close")
-//        }
-//        .padding(.leading, 150).padding(.bottom, 5)
-//      }
-//    }
-//    .background(Color.gray.opacity(0.20))
-//  }
-//}
 
 /**
  Button(action: {
@@ -580,60 +524,6 @@ struct ContentView_Previews: PreviewProvider {
     ContentView().environmentObject(RadioManager())
   }
 }
-
-// MARK: - TextView Wrapper ----------------------------------------------------------------------------
-
-
-//struct TextView: NSViewRepresentable {
-//
-//  @Binding var text: String
-//
-//  func makeCoordinator() -> Coordinator {
-//    Coordinator(self)
-//  }
-//
-//  func makeNSView(context: Context) -> NSTextView {
-//
-//    let myTextView = NSTextView()
-//    myTextView.delegate = context.coordinator
-//
-//    myTextView.font = NSFont(name: "HelveticaNeue", size: 15)
-//    //        myTextView.isScrollEnabled = true
-//    myTextView.isEditable = true
-//    myTextView.backgroundColor = NSColor(white: 0.0, alpha: 0.15)
-//
-//    return myTextView
-//  }
-//
-//  func updateNSView(_ nsView: NSTextView, context: Context) {
-//    nsView.string = text
-//  }
-//
-//  class Coordinator : NSObject, NSTextViewDelegate {
-//
-//    var parent: TextView
-//
-//    init(_ nsTextView: TextView) {
-//      self.parent = nsTextView
-//    }
-//
-//    func textView(_ textView: NSTextView, shouldChangeTextIn range: NSRange, replacementString text: String?) -> Bool {
-//      var newText = textView.string
-//
-//              newText.removeAll { (character) -> Bool in
-//                  return character == " " || character == "\n"
-//              }
-//
-//      return newText.count < 500
-//      //return true
-//    }
-//
-//    func textViewDidChange(_ textView: NSTextView) {
-//      //print("text now: \(String(describing: textView.text!))")
-//      self.parent.text = textView.string
-//    }
-//  }
-//}
 
 
 /*
